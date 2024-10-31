5ddfb36967f98a823cb277f9abc81beea8a0a49b
Yash Bhawsar <yashbhawsar97@gmail.com>
2024-10-31 10:54:03 +0530

    redux

--- learnRedux/src/App.jsx
+++ learnRedux/src/App.jsx
@@ -1,24 +1,9 @@
-// import { useState } from "react";
-
 import "./App.css";
-import ProductList from "./components/ProductList";
-import Cart from "./components/Cart";
-import store from "./components/redux/store";
-import { Provider } from "react-redux";
 
 function App() {
-  // const [cart, setCart] = useState([]);
-
-  // const addToCart = (product) => {
-  //   setCart([...cart, product]);
-  // };
-
   return (
     <>
-      <Provider store={store}>
-        <ProductList />
-        <Cart />
-      </Provider>
+      <h1>Yash</h1>
     </>
   );
 }
