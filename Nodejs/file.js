75231acde142c5b3f75392f1284723ec6f193070
Yash Bhawsar <yashbhawsar97@gmail.com>
2025-01-27 13:32:32 +0530

    file handling

--- Nodejs/file.js
+++ Nodejs/file.js
@@ -2,4 +2,9 @@ const fs = require("fs");
 
 // fs.writeFileSync("./test.txt", "Hey Yash");
 
-fs.writeFile("./test.txt", "Hey Yash async", (err) => {});
+// fs.writeFile("./test.txt", "Hey Yash async", (err) => {});
+
+// const result = fs.readFileSync("./contacts.txt", "utf-8");
+// console.log(result);
+
+const res = fs.appendFileSync("./test.txt", " Yash Bhawsar");
