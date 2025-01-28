70f7b4ab290246cd22eed609bb07216a45d137f8
Yash Bhawsar <yashbhawsar97@gmail.com>
2025-01-28 16:48:23 +0530

    req.url

--- Nodejs/index.js
+++ Nodejs/index.js
@@ -1,3 +1,17 @@
 const http = require("http");
+const fs = require("fs");
 
-const myServer = http.createServer();
+const myServer = http.createServer((req, res) => {
+  const log = `${Date.now()}: ${req.url} New Req Recived\n`;
+  fs.appendFile("log.txt", log, (err, data) => {
+    switch (req.url) {
+      case "/":
+        res.end("It is Home Page");
+        break;
+      case "/about":
+        res.end("Hello I am YAsh Bhawsar");
+    }
+  });
+});
+
+myServer.listen(8000, () => console.log("Server Started"));
