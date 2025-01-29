1b6eaa1117dd8c0033a59a5d327e6d2f7eb69dbe
Yash Bhawsar <yashbhawsar97@gmail.com>
2025-01-29 10:53:17 +0530

    REST Done

--- Nodejs/index.js
+++ Nodejs/index.js
@@ -1,2 +1,47 @@
 const express = require("express");
+const users = require("./MOCK_DATA.json");
+
 const app = express();
+const PORT = 8000;
+
+app.get("/users", (req, res) => {
+  const html = `
+      <ul>
+      ${users.map((user) => `<li> ${user.first_name} </li>`).join("")}
+      </ul>
+      `;
+  res.send(html);
+});
+
+app.get("/api/users", (req, res) => {
+  return res.json(users);
+});
+
+app
+  .route("/api/users/:id")
+  .get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+  })
+  .patch((req, res) => {
+    // Update user with Id
+    return res.json({ status: "pending" });
+  })
+  .delete((req, res) => {
+    // Delete user with Id
+    return res.json({ status: "pending" });
+  });
+
+// app.get("/api/users/:id");
+
+app.post("/api/users", (req, res) => {
+  // Create new user
+  res.json({ status: "pending" });
+});
+
+// app.patch("/api/users/:id");
+
+// app.delete("/api/users/:id");
+
+app.listen(PORT, () => console.log(`Server Started At PORT: ${PORT}`));
