9718d943d9b4ede33bd63a07bd7f0a142cd4eb8e
Yash Bhawsar <yashbhawsar97@gmail.com>
2025-01-29 10:05:50 +0530

    Node API

--- Nodejs/hello.js
+++
@@ -1,4 +0,0 @@
-const { add, sub } = require("./math");
-
-console.log(add(4, 5));
-console.log(sub(4, 5));
--- Nodejs/index.js
+++ Nodejs/index.js
@@ -1,13 +1,2 @@
 const express = require("express");
-
 const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Hello From Home Page");
-});
-
-app.get("/about", (req, res) => {
-  res.send("Hello From Yash");
-});
-
-app.listen(8000, console.log("Server Started"));
