670178c81a1b2ada67c7f2212f29063a693525b6
Yash Bhawsar <yashbhawsar97@gmail.com>
2025-02-02 11:40:59 +0530

    Socket.io

---
+++ Socket.io/index.js
@@ -0,0 +1,16 @@
+const http = require("http");
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(express.static(path.resolve("./public")));
+
+app.get("/", (req, res) => {
+  return res.sendFile("/public/index.html");
+});
+
+server.listen(9000, () => {
+  console.log(`Server is Running on Port 9000`);
+});
