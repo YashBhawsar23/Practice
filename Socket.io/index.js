0bdd9c209f8a6425e3baa600643ac812b78d52e9
Yash Bhawsar <yashbhawsar97@gmail.com>
2025-02-02 13:40:07 +0530

    setup

--- Socket.io/index.js
+++ Socket.io/index.js
@@ -1,9 +1,17 @@
 const http = require("http");
 const express = require("express");
 const path = require("path");
+const { Server } = require("socket.io");
+const { Socket } = require("dgram");
 
 const app = express();
 const server = http.createServer(app);
+const io = new Server(server);
+
+// Socket io
+io.on("connection", (socket) => {
+  console.log("A new User Connected", socket.id);
+});
 
 app.use(express.static(path.resolve("./public")));
 
